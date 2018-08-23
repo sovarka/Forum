5270d4871b118f4ce0677378f92cf1c08d3cdfc2
Barbara <sovarka1@mail.ru>
2018-08-24 00:26:44 +0300

    list remade

--- app/src/main/java/ru/startandroid/newforum/ThemeActivity.java
+++ app/src/main/java/ru/startandroid/newforum/ThemeActivity.java
@@ -27,10 +27,11 @@ public class ThemeActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
     }
 
-    private ArrayList<Message> getMessages(int themeId){
-        return (ArrayList<Message>) Arrays.asList(
-                new Message("asdasd", "ololosh", "1234"),
-                new Message("asdasdasdasd", "tvoya mamka", "12345")
-                ); //TODO get messages from server
+    private ArrayList<Message> getMessages(int themeId) {
+        ArrayList<Message> list = new ArrayList<Message>();
+        list.add(new Message("Kukareky", "Petya", "23.04.17"));
+        list.add(new Message("lalala", "Masha", "11.03.17"));
+        return list;
+         //TODO get messages from server
     }
 }
